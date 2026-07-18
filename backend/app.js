3f16bf30674ec98976f2321ba15f2bb0cd97b86a
procastiaman <amanpanday246@gmail.com>
2026-07-19 00:23:14 +0530

    fixed path

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-// 1. Express ko batao ki frontend folder ke andar saari static files (CSS, Images, JS) hain
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// 2. Jab koi main URL open kare, toh use index.html file serve karo
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', '../frontend/pages/login.html'));
-});
